1b8fb502474f5906a3cd93ab06f6fc79dd8059fd
kivenalaric <elmozaro3@gmail.com>
2026-04-27 13:20:09 +0100

    Add Railway Puppeteer config

--- ghl.js
+++ ghl.js
@@ -45,6 +45,7 @@ async function downloadSignedPdf(viewerUrl) {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
